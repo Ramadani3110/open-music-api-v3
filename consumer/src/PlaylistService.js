1def9b90bde9d42a965879ed98a7cce1bd7899ae
Ramadani3110 <mrama311005@gmail.com>
2023-12-28 20:51:20 +0700

    add exports playlist rabbitmq

---
+++ consumer/src/PlaylistService.js
@@ -0,0 +1,32 @@
+const { Pool } = require("pg");
+
+class PlaylistService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getSongInPlaylist(playlist_id) {
+    const queryPlaylist = {
+      text: "SELECT playlist.id,playlist.name,users.username FROM playlist LEFT JOIN users ON playlist.owner = users.id WHERE playlist.id = $1",
+      values: [playlist_id],
+    };
+
+    const querySongs = {
+      text: "SELECT songs.id,songs.title,songs.performer FROM songs INNER JOIN playlist_songs ON songs.id = playlist_songs.song_id WHERE playlist_songs.playlist_id = $1",
+      values: [playlist_id],
+    };
+
+    const resPlaylist = await this._pool.query(queryPlaylist);
+    const resSongs = await this._pool.query(querySongs);
+
+    return {
+      playlist: {
+        id: resPlaylist.rows[0].id,
+        name: resPlaylist.rows[0].name,
+        songs: resSongs.rows,
+      },
+    };
+  }
+}
+
+module.exports = PlaylistService;
